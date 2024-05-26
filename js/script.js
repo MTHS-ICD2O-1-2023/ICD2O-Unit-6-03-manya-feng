8c079240b9a2492a60dbd334ffe8e054122aae42
Manya Feng <159185413+manya-feng@users.noreply.github.com>
2024-05-26 22:02:47 +0000

    change

--- js/script.js
+++ js/script.js
@@ -1,5 +1,34 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Manya All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Manya
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function gets the Cat fact.
+ * The "async" is there because it will take time for the internet to return the value
+ */
+async function getCatFact() {
+  // the "try" id here because the internet may not be working
+  // it is like an "if ... else" statement"
+  try {
+    const resultJSON = await fetch("https://cat-fact.herokuapp.com/facts")
+    const jsonData = await resultJSON.json()
+    //console.log(jsonData)
+
+    // NOTE: there are 5 (0 to 4) cat facts
+    const randomFactNumber = Math.floor(Math.random() * 5)
+    const firstDataSet = jsonData[randomFactNumber]
+    const textDataSet = firstDataSet.text
+    console.log(textDataSet)
+
+    // output
+    // add 1 to random number, since users would see Fact #0 as odd!
+    document.getElementById("cat-fact-number").innerHTML = "<p>Cat Fact #" + (randomFactNumber + 1) + "</p>"
+    document.getElementById("cat-fact").innerHTML = "<p>" + textDataSet + "</p>"
+  } catch (error) {
+    console.error(error)
+  }
+}
